131c5ee00287068cbfeef5481644b7cbb1a07bb9
jomolin <73376620+jomolin@users.noreply.github.com>
2020-10-24 14:31:10 +0900

    remove seconds

--- script.js
+++ script.js
@@ -9,16 +9,12 @@ function init() {
 };
 
 function clock() {
-    s++;
-    if (s == 60) {
-        s = 0;
-        m++;
-        if (m == 60) {
-            m = 0;
-            h++;
-            if (h == 24) {
-                h = 0;
-            }
+    m++;
+    if (m == 60) {
+        m = 0;
+        h++;
+        if (h == 24) {
+            h = 0;
         }
     }
     $('sec', s);
